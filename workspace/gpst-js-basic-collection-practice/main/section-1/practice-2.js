790d3ea9c6d644a7303c3267c0ae81e9d4cd01a5
rsmaCD <your_email@example.com>
2017-07-18 20:54:08 +0800

    recode practice1.2

--- workspace/gpst-js-basic-collection-practice/main/section-1/practice-2.js
+++ workspace/gpst-js-basic-collection-practice/main/section-1/practice-2.js
@@ -2,17 +2,21 @@
 
 module.exports = function collectSameElements(collectionA, collectionB) {
 
-  var collectionB_array = collectionB[0];
+    var collectionB_array = collectionB[0];
     var result =[];
     for(var i = 0; i < collectionA.length; i++){
-        for(var j = 0; j < collectionB_array.length; j++){
-            if(collectionA[i] == collectionB_array[j]){
-
-                result.push(collectionA[i]);
-            }
+        if(isExist(collectionA[i],collectionB_array)){
+            result.push(collectionA[i]);
         }
     }
-
     return result;
+}
 
+function isExist(element,collection) {
+    for(var i = 0; i < collection.length; i++){
+        if(element == collection[i]) {
+            return true;
+        }
+    }
+    return false;
 }
