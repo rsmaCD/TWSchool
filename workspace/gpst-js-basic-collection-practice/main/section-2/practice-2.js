9bc8b55b54560891fc0bc55a3f58e0ff6d600c89
rsmaCD <your_email@example.com>
2017-07-18 15:58:14 +0800

    real practice2.2

--- workspace/gpst-js-basic-collection-practice/main/section-2/practice-2.js
+++ workspace/gpst-js-basic-collection-practice/main/section-2/practice-2.js
@@ -1,5 +1,46 @@
 'use strict';
 
+
+
 module.exports = function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+    var key = [];
+    var value = [];
+    for(var i = 0;i < collection.length;i++){
+        var reg = '[A-Za-z]-[0-9]+';
+        var temp_key ;
+        var temp_value;
+        if(collection[i].match(reg)){
+            temp_key = collection[i].split("-")[0];
+            temp_value = collection[i].split("-")[1];
+        }else{
+            temp_key = collection[i];
+            temp_value = 1;
+        }
+
+      var flag = -1;
+        for(var j = 0; j < key.length; j++){
+
+            if(temp_key == key[j]) {
+                flag = j;
+                break;
+
+            }
+        }
+        if(flag != -1){
+            value[flag] += temp_value;
+
+        }else{
+            key[key.length] = temp_key;
+            value[key.length-1] = parseInt(temp_value);
+        }
+
+
+    }
+
+    var result = [];
+    for(var i = 0;i < key.length; i++){
+        result.push({"key":key[i],"count":value[i]});
+    }
+
+    return result;
 }
