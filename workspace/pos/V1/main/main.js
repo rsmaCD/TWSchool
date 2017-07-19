167abea5a71a31a34d3f95ec3d1212c6aa3eaddd
rsmaCD <your_email@example.com>
2017-07-19 22:50:24 +0800

    posV1-rename function disCount -> countItemsTotalPrice

--- workspace/pos/V1/main/main.js
+++ workspace/pos/V1/main/main.js
@@ -10,34 +10,59 @@ function printReceipt(tags) {
 
     posV1(tags);
 
-    console.log(`***<没钱赚商店>收据***
-名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)
-名称：荔枝，数量：2.5斤，单价：15.00(元)，小计：37.50(元)
-名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)
-----------------------
-总计：58.50(元)
-节省：7.50(元)
-**********************`);
+//     console.log(`***<没钱赚商店>收据***
+// 名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)
+// 名称：荔枝，数量：2.5斤，单价：15.00(元)，小计：37.50(元)
+// 名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)
+// ----------------------
+// 总计：58.50(元)
+// 节省：7.50(元)
+// **********************`);
 
 }
 
 function posV1(tags){
     let itemsIdAndCountList = getItemsAndCount(tags);
 
-    let itemsAndCountList = addItemProperty(itemsIdAndCountList);
+    let itemsAndCountList = addItemsProperty(itemsIdAndCountList);
 
-    itemsAndCountList = disCount(itemsAndCountList);
-    
-    print(itemsAndCountList);
+    itemsAndCountList = countItemsTotalPrice(itemsAndCountList);
 
-}
+    let totalPriceAndSavePrice = getTotalPriceAndSavePrice(itemsAndCountList);
 
-function print(itemsAndCountList) {
-
+    printResult(itemsAndCountList,totalPriceAndSavePrice);
 
 }
 
-function addItemProperty(itemsIdAndCountList){
+function printResult(itemsAndCountList,totalPriceAndSavePriace) {
+    let result = "";
+    result += "***<没钱赚商店>收据***\r\n";
+    for(let i = 0;i < itemsAndCountList.length;i++){
+        result += "名称：" + itemsAndCountList[i].itemProperty.name
+            + "，数量：" + itemsAndCountList[i].count + itemsAndCountList[i].itemProperty.unit
+            +"，单价：" + itemsAndCountList[i].itemProperty.price.toFixed(2) + "(元)"
+            +"，小计：" + itemsAndCountList[i].itemPrice.toFixed(2) + "(元)\r\n";
+    }
+    result += "----------------------\r\n";
+
+    result += "总计：" + totalPriceAndSavePriace.totalPrice.toFixed(2) + "(元)\r\n";
+    result += "节省：" + totalPriceAndSavePriace.savePrice.toFixed(2) + "(元)\r\n";
+
+    result += "**********************";
+    console.log(result);
+}
+
+function getTotalPriceAndSavePrice(itemsAndCountList) {
+    let totalPrice = 0;
+    let savePrice = 0;
+    for(let i = 0;i < itemsAndCountList.length;i++){
+        totalPrice += itemsAndCountList[i].itemPrice;
+        savePrice += (itemsAndCountList[i].itemProperty.price *itemsAndCountList[i].count) -itemsAndCountList[i].itemPrice;
+    }
+    return {"totalPrice":totalPrice,"savePrice":savePrice};
+}
+
+function addItemsProperty(itemsIdAndCountList){
     for(let i = 0; i < itemsIdAndCountList.length; i++){
         itemsIdAndCountList[i].itemProperty = getOneItemsById(itemsIdAndCountList[i].itemId);
         itemsIdAndCountList[i].isDiscount = isDiscountById(itemsIdAndCountList[i].itemId);
@@ -73,9 +98,9 @@ function getItemsAndCount(tags) {
 }
 
 
-function disCount(itemsAndCountList) {
+function countItemsTotalPrice(itemsAndCountList) {
     for(let i = 0; i < itemsAndCountList.length; i++){
-        itemsAndCountList[i].itemPriace = getItemTotalPrice(itemsAndCountList[i]);
+        itemsAndCountList[i].itemPrice = getItemTotalPrice(itemsAndCountList[i]);
     }
     return itemsAndCountList;
 }
