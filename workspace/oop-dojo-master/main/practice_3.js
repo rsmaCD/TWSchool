1cf771e7e3c235a1f486f3138b026b23a2a286d1
rsmaCD <your_email@example.com>
2017-07-21 15:59:18 +0800

    practice3 test pass 预估10min,实际9min

---
+++ workspace/oop-dojo-master/main/practice_3.js
@@ -0,0 +1,22 @@
+
+class Person {
+    constructor(name,age){
+        this.name = name;
+        this.age = age;
+    }
+    introduce(){
+        return `My name is ${this.name}. I am ${this.age} years old.`;
+    }
+}
+
+export default class Student extends Person{
+
+    constructor(name,age,classNum){
+        super(age, name);
+        this.classNum = classNum;
+    }
+
+    introduce(){
+        return `I am a Student. I am at Class ${this.classNum}.`;
+    }
+}
--- workspace/oop-dojo-master/spec/practice_3/main_spec.js
+++ workspace/oop-dojo-master/spec/practice_3/main_spec.js
@@ -1,7 +1,9 @@
 "use strict";
+import Student from '../../main/practice_3';
 
 describe("OO入门", function(){
     it("覆盖父类的方法-1", function(){
-    
+        const student = new Student('Tom',21,2);
+        expect(student.introduce()).toEqual("I am a Student. I am at Class 2.");
     });
 });
