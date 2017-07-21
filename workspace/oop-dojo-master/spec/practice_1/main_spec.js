f0868a778dce867f05471880071ce6ca8a204933
rsmaCD <your_email@example.com>
2017-07-21 15:30:39 +0800

    practice1 test pass 预估5min 实际20min

--- workspace/oop-dojo-master/spec/practice_1/main_spec.js
+++ workspace/oop-dojo-master/spec/practice_1/main_spec.js
@@ -1,7 +1,12 @@
 "use strict";
 
+import Person from '../../main/practice_1';
+
 describe("OO入门", function(){
     it("写一个类-1", function(){
 
+        const person = new Person("Tom",21);
+
+        expect(person.introduce()).toEqual("My name is Tom. I am 21 years old.");
     });
 });
