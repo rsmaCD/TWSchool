b9939729f045c8950529d14d508038644904d947
rsmaCD <your_email@example.com>
2017-07-21 17:20:03 +0800

    practice6 test pass 预估15min,实际20min

---
+++ workspace/oop-dojo-master/main/practice_6.js
@@ -0,0 +1,38 @@
+class Person {
+    constructor(name,age){
+        this.name = name;
+        this.age = age;
+    }
+    introduce(){
+        return `My name is ${this.name}. I am ${this.age} years old.`;
+    }
+}
+
+class Student extends Person{
+
+    constructor(name,age,classNum){
+        super(name, age);
+        this.classNum = classNum;
+    }
+
+    introduce(){
+        return super.introduce() + " " + `I am a Student. I am at Class ${this.classNum}.`;
+    }
+}
+
+class Worker extends Person{
+
+    constructor(name,age){
+        super(name, age);
+    }
+
+    introduce(){
+        return super.introduce() + " " + `I am a Worker. I have a job.`;
+    }
+
+}
+module.exports =  {
+    Person,
+    Student,
+    Worker
+}
