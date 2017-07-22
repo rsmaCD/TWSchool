4bb48301484636bd4ee8a0d405a1a41f798819b2
rsmaCD <your_email@example.com>
2017-07-22 22:27:30 +0800

    practice11 test1 pass

--- workspace/oop-dojo-master/spec/practice_11/main_spec.js
+++ workspace/oop-dojo-master/spec/practice_11/main_spec.js
@@ -2,6 +2,20 @@
 
 describe("OO入门", function(){
     it("对象之间的交互-5", function(){
+        it("对象之间的交互-4", function(){
+            const person = new Person(110,'Tom',21);
+            expect(person.introduce()).toEqual('My name is Tom. I am 21 years old.');
 
+            const classObj = new Class(2);
+            const student = new Student(110,'Tom',21,classObj);
+            expect(student.introduce()).toEqual('My name is Tom. I am 21 years old. I am a Student. I am at Class 2.');
+
+            classObj.assignLeader(student);
+            expect(student.introduce()).toEqual('My name is Tom. I am 21 years old. I am a Student. I am Leader of Class 2.');
+
+            const student2 = new Student(111,'Anna',22);
+            expect(classObj.assignLeader(student2)).toEqual('It is not one of us.');
+
+        });
     });
 });
