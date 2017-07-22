6e67d838d6da05652f3879b2d9203b5715808f76
rsmaCD <your_email@example.com>
2017-07-22 18:37:28 +0800

    practice8 test1 pass (function introduce)

---
+++ workspace/oop-dojo-master/main/practice_8.js
@@ -0,0 +1,55 @@
+class Person {
+    constructor(name,age){
+        this.name = name;
+        this.age = age;
+    }
+    introduce(){
+        return `My name is ${this.name}. I am ${this.age} years old.`;
+    }
+}
+
+class ClassObj{
+    constructor(classNum){
+        this.classNum = classNum;
+    }
+    getClassNum(){
+        return this.classNum;
+    }
+}
+
+class Student extends Person{
+
+    constructor(name,age,classNum){
+        super(name, age);
+        this.class = classNum;
+    }
+
+    introduce(){
+        return super.introduce() + " " + `I am a Student. I am at Class ${this.class.getClassNum()}.`;
+    }
+}
+
+class Teacher extends Person{
+
+    constructor(name,age,classNum){
+        super(name,age);
+        this.class = classNum;
+
+    }
+
+    introduce(){
+        if(this.class != undefined){
+            return super.introduce() + " " + `I am a Teacher. I teach Class ${this.class.getClassNum()}.`;
+        }else{
+            return super.introduce() + " " + `I am a Teacher. I teach No Class.`;
+        }
+
+    }
+}
+
+module.exports =  {
+    Person,
+    Student,
+    Teacher,
+    ClassObj
+}
--- workspace/oop-dojo-master/spec/practice_8/main_spec.js
+++ workspace/oop-dojo-master/spec/practice_8/main_spec.js
@@ -1,7 +1,15 @@
 "use strict";
-
+import {Person,Student,Teacher,ClassObj} from '../../main/practice_8';
 describe("OO入门", function(){
     it("对象之间的交互-2", function(){
-
+        const classObj = new ClassObj(2);
+        const person = new Person('Tom',21);
+        const student = new Student('Tom',21,classObj);
+        const teacher1 = new Teacher('Tom',21,classObj);
+        const teacher2 = new Teacher('Tom',21);
+        expect(person.introduce()).toEqual('My name is Tom. I am 21 years old.');
+        expect(student.introduce()).toEqual('My name is Tom. I am 21 years old. I am a Student. I am at Class 2.');
+        expect(teacher1.introduce()).toEqual('My name is Tom. I am 21 years old. I am a Teacher. I teach Class 2.');
+        expect(teacher2.introduce()).toEqual('My name is Tom. I am 21 years old. I am a Teacher. I teach No Class.');
     });
 });
