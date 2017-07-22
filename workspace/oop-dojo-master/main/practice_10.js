17e55391e72092e9f7b3a98eec1eaddb692cc6e0
rsmaCD <your_email@example.com>
2017-07-22 22:13:50 +0800

    practice10 test1 pass

---
+++ workspace/oop-dojo-master/main/practice_10.js
@@ -0,0 +1,62 @@
+class Person{
+    constructor(id,name,age){
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+    introduce(){
+        return `My name is ${this.name}. I am ${this.age} years old.`;
+    }
+}
+
+class Class{
+    constructor(classNum){
+        this.number = classNum;
+    }
+    getClassNum(){
+        return this.number;
+    }
+
+    assignLeader(student){
+        if(student.getClass() !== this){
+            return `It is not one of us.`;
+        }else{
+            this.leader = student;
+        }
+    }
+
+    getClassLeader(){
+        return this.leader;
+    }
+
+    appendMember(student){
+        student.setClass(this);
+    }
+}
+
+class Student extends Person{
+    constructor(id,name,age,classObj){
+        super(id,name,age);
+        this.class = classObj;
+    }
+
+    setClass(classObj){
+        this.class = classObj;
+    }
+
+    getClass(){
+        return this.class;
+    }
+    introduce(){
+        if(this.class.getClassLeader() === this){
+            return super.introduce() + ` I am a Student. I am Leader of Class ${this.class.getClassNum()}.`;
+        }
+        return super.introduce() + ` I am a Student. I am at Class ${this.class.getClassNum()}.`;
+    }
+}
+
+module.exports = {
+    Person,
+    Class,
+    Student
+}
