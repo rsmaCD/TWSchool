27c322b17168130a408f679f75d33670ed43c926
rsmaCD <your_email@example.com>
2017-07-24 16:08:39 +0800

    practice_4 test2,3 pass finished 预估20min,实际30min

---
+++ workspace/oop-dojo-java-master/src/main/java/practice_4/Student.java
@@ -0,0 +1,20 @@
+package practice_4;
+
+/**
+ * Created by rsma on 24/07/2017.
+ */
+public class Student {
+    private int kclass;
+    private String name;
+    private int age;
+
+    public Student(String name, int age, int kclass) {
+        this.name = name;
+        this.age = age;
+        this.kclass = kclass;
+    }
+
+    public String introduce() {
+        return "My name is " + this.name + ". I am " + this.age +" years old. I am a Student. I am at Class " + this.kclass + ".";
+    }
+}
---
+++ workspace/oop-dojo-java-master/src/test/java/practice_4/StudentTest.java
@@ -0,0 +1,16 @@
+package practice_4;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * Created by rsma on 24/07/2017.
+ */
+public class StudentTest {
+    @Test
+    public void tsetIntroduce() throws Exception {
+        Student student = new Student("Tom", 21, 2);
+        assertEquals(student.introduce(),"My name is Tom. I am 21 years old. I am a Student. I am at Class 2.");
+    }
+}
