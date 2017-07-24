70f0dea84dffe0ac33165b531181cf3ed1557817
rsmaCD <your_email@example.com>
2017-07-24 15:56:27 +0800

    practice_2 fixed

--- workspace/oop-dojo-java-master/src/main/java/practice_3/Student.java
+++ workspace/oop-dojo-java-master/src/main/java/practice_3/Student.java
@@ -5,18 +5,18 @@ import practice_2.Person;
 /**
  * Created by rsma on 24/07/2017.
  */
-public class Student extends Person {
+public class Student{
     private int kclass;
     private String name;
     private int age;
 
     public Student(String name, int age, int kclass) {
-        super(name,age);
+        this.name = name;
+        this.age = age;
         this.kclass = kclass;
     }
 
-    @Override
     public String introduce() {
-        return super.introduce() + " I am a Student. I am at Class " + this.kclass + ".";
+        return "My name is " + this.name + ". I am " + this.age +" years old. I am a Student. I am at Class " + this.kclass + ".";
     }
 }
