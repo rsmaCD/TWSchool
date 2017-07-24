ef671baca4c90a8826315f686773428bb2420404
rsmaCD <your_email@example.com>
2017-07-24 17:44:46 +0800

    practice_8 test1 pass

---
+++ workspace/oop-dojo-java-master/src/main/java/practice_8/Person.java
@@ -0,0 +1,18 @@
+package practice_8;
+
+/**
+ * Created by rsma on 24/07/2017.
+ */
+public class Person {
+    protected int age;
+    protected String name;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String introduce() {
+        return "My name is " + this.name + ". I am " + this.age + " years old.";
+    }
+}
