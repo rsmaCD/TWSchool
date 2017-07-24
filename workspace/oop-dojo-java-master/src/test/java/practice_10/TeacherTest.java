597a56b5cc77ecf2baa829a03310253d33e8264c
rsmaCD <your_email@example.com>
2017-07-24 21:40:54 +0800

    practice_10 f 预估20min，实际30min

--- workspace/oop-dojo-java-master/src/test/java/practice_10/TeacherTest.java
+++ workspace/oop-dojo-java-master/src/test/java/practice_10/TeacherTest.java
@@ -1,4 +1,4 @@
-package practice_9;
+package practice_10;
 
 import org.junit.Test;
 
@@ -13,9 +13,9 @@ public class TeacherTest {
 
     @Test
     public void testIntroduce() throws Exception {
-        kclass = new KClass(2);
+        kclass = new KClass(3);
         Teacher teacher = new Teacher("Tom", 21, kclass);
-        assertEquals(teacher.introduce(),"My name is Tom. I am 21 years old. I am a Teacher. I teach Class 2.");
+        assertEquals(teacher.introduce(),"My name is Tom. I am 21 years old. I am a Teacher. I teach Class 3.");
 
         Teacher teacher1 = new Teacher("Tom",21);
         assertEquals(teacher1.introduce(),"My name is Tom. I am 21 years old. I am a Teacher. I teach No Class.");
