44ce18696db68a96fd20d37b15371912ecb67db5
rsmaCD <your_email@example.com>
2017-07-24 20:06:52 +0800

    practice_9 finished 预估20min,实际22min

---
+++ workspace/oop-dojo-java-master/src/main/java/practice_9/Teacher.java
@@ -0,0 +1,26 @@
+package practice_9;
+
+/**
+ * Created by rsma on 24/07/2017.
+ */
+public class Teacher extends Person {
+
+    private KClass kclass = null;
+
+    public Teacher(String name, int age, KClass kclass) {
+        super(name, age);
+        this.kclass = kclass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name,age);
+    }
+
+    @Override
+    public String introduce() {
+        if(this.kclass != null){
+            return super.introduce() + " I am a Teacher. I teach Class " + this.kclass.getClassNum() + ".";
+        }
+        return super.introduce() + " I am a Teacher. I teach No Class.";
+    }
+}
---
+++ workspace/oop-dojo-java-master/src/test/java/practice_9/TeacherTest.java
@@ -0,0 +1,23 @@
+package practice_9;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * Created by rsma on 24/07/2017.
+ */
+public class TeacherTest {
+
+    private KClass kclass;
+
+    @Test
+    public void testIntroduce() throws Exception {
+        kclass = new KClass(2);
+        Teacher teacher = new Teacher("Tom", 21, kclass);
+        assertEquals(teacher.introduce(),"My name is Tom. I am 21 years old. I am a Teacher. I teach Class 2.");
+
+        Teacher teacher1 = new Teacher("Tom",21);
+        assertEquals(teacher1.introduce(),"My name is Tom. I am 21 years old. I am a Teacher. I teach No Class.");
+    }
+}
