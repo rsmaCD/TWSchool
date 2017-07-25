bb4c7706a617ca1f5d8b78b5ff0ddbb9d0291ee4
rsmaCD <your_email@example.com>
2017-07-25 16:50:58 +0800

    FrequencyWords test1(should_get_words_and_frequency_when_call_countwords)pass

--- workspace/tdd-java-base-master/src/main/java/frequencyWords/FrequencyWords.java
+++ workspace/tdd-java-base-master/src/main/java/frequencyWords/FrequencyWords.java
@@ -1,6 +1,7 @@
 package frequencyWords;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -18,5 +19,20 @@ public class FrequencyWords {
             }
         }
         return strList;
+    }
+
+    public HashMap<String,Integer> countWords(List<String> strList) {
+
+        HashMap<String,Integer> wordsAndFrequency = new HashMap<>();
+        for (String strTemp : strList) {
+            if(wordsAndFrequency.containsKey(strTemp)){
+                int value = wordsAndFrequency.get(strTemp);
+                wordsAndFrequency.put(strTemp,value + 1);
+            }else {
+                wordsAndFrequency.put(strTemp,1);
+            }
+        }
+
+        return wordsAndFrequency;
     }
 }
--- workspace/tdd-java-base-master/src/test/java/frequencyWords/FrequencyWordsTest.java
+++ workspace/tdd-java-base-master/src/test/java/frequencyWords/FrequencyWordsTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 import static junit.framework.TestCase.assertEquals;
@@ -14,15 +15,34 @@ import static org.hamcrest.core.Is.is;
  */
 public class FrequencyWordsTest {
 
+    private List<String> strList;
+    private FrequencyWords frequencyWords;
+
     @Test
     public void should_get_string_list_when_call_splitstring() throws Exception {
         //Given
         String str = "a the a nihao  kk";
-        FrequencyWords frequencyWords = new FrequencyWords();
+        frequencyWords = new FrequencyWords();
         //When
-        List<String> actualList = frequencyWords.splitStr(str);
+        List<String> exceptList = frequencyWords.splitStr(str);
         //Then
-        List<String> strList = Arrays.asList("a","the","a","nihao","kk");
-        assertEquals(actualList,strList);
+        strList = Arrays.asList("a","the","a","nihao","kk");
+        assertEquals(strList, exceptList);
+    }
+
+    @Test
+    public void should_get_words_and_frequency_when_call_countwords() throws Exception {
+        //Given
+        strList = Arrays.asList("a","the","a","nihao","kk");
+        frequencyWords = new FrequencyWords();
+        //When
+        HashMap<String,Integer> exceptHashMap = frequencyWords.countWords(strList);
+        //Then
+        HashMap<String,Integer> actualHashMap = new HashMap<>();
+        actualHashMap.put("a",2);
+        actualHashMap.put("the",1);
+        actualHashMap.put("nihao",1);
+        actualHashMap.put("kk",1);
+        assertEquals(actualHashMap,exceptHashMap);
     }
 }
