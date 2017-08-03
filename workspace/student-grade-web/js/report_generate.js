0a399f8e36aea022c449101e7dae02fa6dfcb0eb
rsmaCD <your_email@example.com>
2017-08-03 22:53:21 +0800

    [rsma]refactor search stu js

--- workspace/student-grade-web/js/report_generate.js
+++ workspace/student-grade-web/js/report_generate.js
@@ -3,9 +3,11 @@
  */
 $(function () {
 
-    var report_info = {};
+    var report_info_all = {};
+    var report_info_one = {};
 
     var report_item_list = [];
+    var report_item_list_one = [];
 
     var report_item_1 = {
         "stu_name":"lili",
@@ -38,23 +40,49 @@ $(function () {
     report_item_list.push(report_item_2);
     report_item_list.push(report_item_3);
 
-    report_info = {
+    report_info_all = {
         "report_item_list":report_item_list,
         "average":33,
         "median":244
     }
 
-    localStorage.setItem("report_info", JSON.stringify(report_info));
 
-    show_report();
+    report_item_list_one.push(report_item_1);
+
+    report_info_one = {
+        "report_item_list":report_item_list_one,
+        "average":89,
+        "median":89
+    }
+
+    localStorage.setItem("report_info_all", JSON.stringify(report_info_all));
+    localStorage.setItem("report_info_one", JSON.stringify(report_info_one));
+
+    show_report("report_info_all");
+
+    $("#search-id-button").click(function(){
+        var stu_num = $("#stu_id").val().trim();
+        alert(stu_num);
+        if(localStorage.getItem('stu_info_list') && stu_num != ""){
+            var stu_info_list = jQuery.parseJSON(localStorage.getItem('stu_info_list'));
+            var stu_arr = [];
+            $.each(stu_info_list, function(i,stu){
+                if(stu.stu_id == stu_num){
+                    show_report("report_info_one");
+                }
+            });
+        }
+
+    });
 
 });
 
-show_report = function () {
-    if (localStorage.getItem('stu_info_list')) {
-        var report_info = jQuery.parseJSON(localStorage.getItem('report_info'));
+show_report = function (report_info) {
+    if (localStorage.getItem(report_info)) {
+        var report_info = jQuery.parseJSON(localStorage.getItem(report_info));
         $('#report_result').html("");
         $('#report_template').tmpl(report_info).appendTo('#report_result');
+        $('#report_total_and_average').html("");
         $('#report_total_and_average_template').tmpl(report_info).appendTo('#report_total_and_average');
     }
 };
--- workspace/student-grade-web/js/search_stu.js
+++ workspace/student-grade-web/js/search_stu.js
@@ -6,34 +6,14 @@ $(function () {
 
     $("#search-id-button").click(function(){
         var stu_num = $("#stu_id").val().trim();
-        if(localStorage.getItem('stu_info_list') && stu_num != ""){
-            var stu_info_list = jQuery.parseJSON(localStorage.getItem('stu_info_list'));
-            var stu_arr = [];
-            $.each(stu_info_list, function(i,stu){
-                if(stu.stu_id == stu_num){
-                    stu_arr.push(stu);
-                }
-            });
-            remove_student_from_result_table();
-            append_to_search_result_table(stu_arr);
-        }
-
+        search_stu("stu_id",stu_num);
+        $("#stu_name").val("");
     });
 
     $("#search-name-button").click(function(){
         var stu_name = $("#stu_name").val().trim();
-        if(localStorage.getItem('stu_info_list') && stu_name != ""){
-            var stu_info_list = jQuery.parseJSON(localStorage.getItem('stu_info_list'));
-            var stu_arr = [];
-            $.each(stu_info_list, function(i,stu){
-                if(stu.stu_name == stu_name){
-                    stu_arr.push(stu);
-                }
-            });
-            remove_student_from_result_table();
-            append_to_search_result_table(stu_arr);
-        }
-
+        search_stu("stu_name",stu_name);
+        $("#stu_id").val("");
     });
 
 });
@@ -41,16 +21,26 @@ $(function () {
 show_all_stu = function () {
     if (localStorage.getItem('stu_info_list')) {
         var stu_info_list = jQuery.parseJSON(localStorage.getItem('stu_info_list'));
-        remove_student_from_result_table();
         append_to_search_result_table(stu_info_list);
     }
 };
 
 append_to_search_result_table = function (stu_info_list) {
+    $('#search_result').html("");
     var search_result = {"data": stu_info_list};
     $('#search_result_template').tmpl(search_result).appendTo('#search_result');
 };
 
-remove_student_from_result_table = function () {
-    $('#search_result').html("");
+
+search_stu = function (stu_filed_name,stu_filed_value) {
+    if(localStorage.getItem('stu_info_list') && stu_filed_value != ""){
+        var stu_info_list = jQuery.parseJSON(localStorage.getItem('stu_info_list'));
+        var stu_arr = [];
+        $.each(stu_info_list, function(i,stu){
+            if(stu[stu_filed_name] == stu_filed_value){
+                stu_arr.push(stu);
+            }
+        });
+        append_to_search_result_table(stu_arr);
+    }
 }
