a003fa41318af104bda1ed5564f8f56c6c55e467
rsmaCD <your_email@example.com>
2017-08-08 19:08:52 +0800

    [rsma]finished search student by id

--- workspace/student-grade-web/js/search_stu_server.js
+++ workspace/student-grade-web/js/search_stu_server.js
@@ -4,17 +4,17 @@
 $(function () {
     show_all_stu();
 
-    $("#search-id-button").click(function(){
+    $("#search-id-button").click(function () {
         var stu_num = $("#stu_id").val().trim();
-        search_stu("stu_id",stu_num);
+        search_stu(stu_num);
         $("#stu_name").val("");
     });
 
-    $("#search-name-button").click(function(){
-        var stu_name = $("#stu_name").val().trim();
-        search_stu("stu_name",stu_name);
-        $("#stu_id").val("");
-    });
+    // $("#search-name-button").click(function(){
+    //     var stu_name = $("#stu_name").val().trim();
+    //     search_stu("stu_name",stu_name);
+    //     $("#stu_id").val("");
+    // });
 
 });
 
@@ -28,10 +28,10 @@ show_all_stu = function () {
         url: "http://localhost:8080/students",
         type: "GET",
         contentType: "application/json; charset=utf-8",
-    }).then(function(data, status, jqxhr) {
+    }).then(function (data, status, jqxhr) {
         console.log(data);
         console.log(status);
-        if(status =="success"){
+        if (status == "success") {
             append_to_search_result_table(data);
         }
     });
@@ -44,15 +44,28 @@ append_to_search_result_table = function (stu_info_list) {
 };
 
 
-search_stu = function (stu_filed_name,stu_filed_value) {
-    if(localStorage.getItem('stu_info_list') && stu_filed_value != ""){
-        var stu_info_list = jQuery.parseJSON(localStorage.getItem('stu_info_list'));
-        var stu_arr = [];
-        $.each(stu_info_list, function(i,stu){
-            if(stu[stu_filed_name] == stu_filed_value){
-                stu_arr.push(stu);
-            }
-        });
-        append_to_search_result_table(stu_arr);
-    }
-}
+search_stu = function (stu_num) {
+
+    $.ajax({
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        url: "http://localhost:8080/students/" + stu_num,
+        contentType: "application/json; charset=utf-8",
+        type: 'GET',
+        success: function (data, textStatus) {
+            console.log(data);
+            console.log(status);
+            alert("11111")
+            var stu_arr = [];
+            stu_arr.push(data);
+            append_to_search_result_table(stu_arr);
+        },
+        error: function () {
+            alert("111")
+            $('#search_result').html("");
+        }
+    });
+
+};
